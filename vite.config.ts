bcc5fdf1997d0e860967939951db44074873c769
knightterris <agkaungmyat19@gmail.com>
2026-09-22 14:25:08 +0700

    vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    base: process.env.VITE_BASE_PATH || '/',
+
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
 
     resolve: {
       alias: {
